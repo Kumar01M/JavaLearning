45064f3181f9f76533fe95ddae70cd2a62965cf0
Kumar <104199783+Kumar01M@users.noreply.github.com>
2022-09-07 22:16:14 +0530

    Add files via upload

---
+++ Branching&Looping/SumOfNum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class SumOfNum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int i = in.nextInt();	//number
+        int n = in.nextInt();	//times
+        while(n--!=0)i+=i;
+        System.out.println(i);
+        in.close();
+    }
+}
