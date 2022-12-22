ce2ae0d98b115a4d1e250f96cc759df90ffeecf0
Kumar <104199783+Kumar01M@users.noreply.github.com>
2022-12-22 07:32:51 +0530

    Update IdUniEleUsingXOR.java

--- BitwiseOperators/IdUniEleUsingXOR.java
+++ BitwiseOperators/IdUniEleUsingXOR.java
@@ -1,26 +1,22 @@
 public class IdUniEleUsingXOR{
 
     public static void main(String[] args){
-
+        int sum = 0;
         int[] arr = new int[] {1, 2, 5, 3, 4, 3, 2, 1};
-
-        boolean flag = true;
-
-        for(int i=0;i<arr.length;i++){
-
-            for(int j=0;j<arr.length;j++){
-
-                if(i==j)continue;
-
-                if((arr[i]^arr[j])==0) flag = false;
-
-            }
-
-            if(flag)System.out.println(arr[i]+" is the unique element.");
-
-            flag = true;
-
+        for (int val: arr) {
+            sum ^= val;
         }
+        System.out.print(sum); //can find only if there's one unique
+        
+//         boolean flag = true;
+//         for(int i=0;i<arr.length;i++){
+//             for(int j=0;j<arr.length;j++){
+//                 if(i==j)continue;
+//                 if((arr[i]^arr[j])==0) flag = false;
+//             }
+//             if(flag)System.out.println(arr[i]+" is the unique element.");
+//             flag = true;
+//         }
 
     }
 
