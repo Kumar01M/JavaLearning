0f3f483acde69268b1dba0e74d027a409015df40
Kumar <104199783+Kumar01M@users.noreply.github.com>
2022-12-22 07:39:34 +0530

    Update NestedIf.java

--- Branching&Looping/NestedIf.java
+++ Branching&Looping/NestedIf.java
@@ -1,32 +1,23 @@
 import java.util.Scanner;
 
-
-
 public class NestedIf {
-
     
     public static void main(String[] args) {
-
         Scanner in = new Scanner(System.in);
-
         System.out.println("Enter the Registration Number: ");
-
         int n = in.nextInt();
-
-        if(n>=1&&n<=100){
-
-            if(n<=25) System.out.println("Go to Exam Hall A");
-
-            else if(n<=50) System.out.println("Go to Exam Hall B");
-
-            else if(n<=75) System.out.println("Go to Exam Hall C");
-
-            else System.out.println("Go to Exam Hall D");
-
+        if (n>=1&&n<=100){
+            if (n<=25)
+                System.out.println("Go to Exam Hall A");
+            else if (n<=50)
+                System.out.println("Go to Exam Hall B");
+            else if (n<=75)
+                System.out.println("Go to Exam Hall C");
+            else
+                System.out.println("Go to Exam Hall D");
         }
- 	else System.out.println("Enter valid registration number");
-
+ 	    else
+            System.out.println("Enter valid registration number");
         }
-
     }
     
