e636b3d0ec99e55176ee4324e50aa23a6fd84f01
Kumar <104199783+Kumar01M@users.noreply.github.com>
2022-12-22 07:40:38 +0530

    Update SumOfChess.java

--- Branching&Looping/SumOfChess.java
+++ Branching&Looping/SumOfChess.java
@@ -1,16 +1,11 @@
 import java.math.*;
 
-
 public class SumOfChess {
-
+    
     public static void main(String[] args) {
-
         BigInteger A = BigInteger.ZERO;
-
-        for(int i = 0;i<64;i++)A = A.add(BigInteger.valueOf(2).pow(Integer.valueOf(i)));
-
+        for(int i = 0;i<64;i++)
+            A = A.add(BigInteger.valueOf(2).pow(Integer.valueOf(i)));
         System.out.println(A);
-
     }
-
 }
